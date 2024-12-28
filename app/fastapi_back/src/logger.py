25a666d76202129b530fd9777fa33268d605b21b
Albert <tayar0409@gmail.com>
2024-12-28 20:48:06 +0300

    pretrained tf-idf

--- app/fastapi_back/src/logger.py
+++ app/fastapi_back/src/logger.py
@@ -4,25 +4,18 @@ from logging.handlers import RotatingFileHandler
 
 
 def setup_logger(name, log_file, level=logging.INFO):
-    """Функция для настройки логгера"""
     formatter = logging.Formatter(
         '%(asctime)s %(levelname)s: %(message)s [in %(pathname)s:%(lineno)d]')
-
     handler = RotatingFileHandler(log_file, maxBytes=10000000, backupCount=5)
     handler.setFormatter(formatter)
-
     logger = logging.getLogger(name)
     logger.setLevel(level)
     logger.addHandler(handler)
-
     return logger
 
-
-# Создаем директорию для логов, если она не существует
 if not os.path.exists('logs'):
     os.makedirs('logs')
 
-# Настраиваем логгеры для разных модулей
 main_logger = setup_logger('main', 'logs/main.log')
 api_logger = setup_logger('api', 'logs/api.log')
 qdrant_logger = setup_logger('qdrant', 'logs/qdrant.log')
