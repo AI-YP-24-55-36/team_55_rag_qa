17e1a4ba5b2734e06116b472a29d4914e497a2c1
Lyudmila Teplova <lteplova@gmail.com>
2024-12-29 13:49:10 +0300

    load dataset

--- app/streamlit/st_app.py
+++ app/streamlit/st_app.py
@@ -2,6 +2,7 @@ import streamlit as st
 from validate_df import validate_csv
 from eda import plot_length, length, plot_top_words, plot_wordcloud, prep, plot_tsne
 from project_logger import log_and_display
+import requests
 
 st.set_page_config(
     page_title="RAG",
@@ -10,9 +11,12 @@ st.set_page_config(
     initial_sidebar_state="expanded",
 )
 
+API_URL = "http://127.0.0.1:8000/api/v1/models"
+
 
 def main():
     # загрузка заголовка приложения
+    global payloads
     st.title("Обучение модели для чат-бота на основе RAG")
     st.markdown("""
     ### Возможности приложения
@@ -40,7 +44,12 @@ def main():
             log_and_display("Файл успешно загружен и проверен!", level="success", display_func=st.success)
             data = valid_df
             st.write("Превью - первые 5 строк:")
-            # data = data[:5000]
+            # переименование колонок в соотвествии с требованием бэка
+            data = data.rename(columns={
+                data.columns[0]: 'context',
+                data.columns[1]: 'question',
+                data.columns[2]: 'answer'
+            })
             st.dataframe(data.head(5))
 
         if valid_df is not None:
@@ -89,8 +98,6 @@ def main():
                         st.session_state[key] = False
 
             if uploaded_file is not None:
-
-
                 if st.sidebar.checkbox("Длины текстов", key="graph1", on_change=clear_other_checkboxes,
                                        args=("graph1",)):
                     new_data = length(data)
@@ -125,10 +132,13 @@ def main():
                     st.pyplot(fig)
 
             st.sidebar.title("Препроцессинг")
+            st.sidebar.subheader("удаление дубликатов и пропусков, предобработка текста и отправка данных на сервер")
 
             if uploaded_file is not None:
-                if st.sidebar.button("Удалить дубликаты"):
+
+                if st.sidebar.button("Очистить и отправить"):
                     columns = data.columns
+                    data = data[:100]
                     if data.duplicated().values.any():
                         data.drop_duplicates(keep='first', inplace=True)
                         log_and_display(f"Строки с дубликатами удалены. Стало всего {len(data)} строк", level="success",
@@ -136,180 +146,188 @@ def main():
                     else:
                         st.warning("В датасете нет дубликатов")
 
-            if uploaded_file is not None:
-                if st.sidebar.button("Удалить пропуски"):
                     if data.isnull().values.any():
                         data = data.dropna()
                         log_and_display(f"Строки с пропусками удалены. Стало всего {len(data)} строк", level="success",
                                         display_func=st.success)
                     else:
                         st.warning("В датасете нет пропущенных значений")
+                    data["context"] = data[columns[0]].apply(lambda x: prep(x))
+                    data["question"] = data[columns[1]].apply(lambda x: prep(x))
+                    data["answer"] = data[columns[2]].apply(lambda x: prep(x))
 
-            if uploaded_file is not None:
-                if st.sidebar.button("Очистить текст"):
-                    cols = data.columns
-                    data["clear_text"] = data[cols[0]].apply(lambda x: prep(x))
                     log_and_display(
-                        f"Текст очищен от стоп-слов, от символов не являющихся буквами и цифрами, приведен к нижнему регистру, эту колонку можно подавать в модель для обучения",
-                        level="success",
+                        f"Текст очищен от стоп-слов, от символов не являющихся буквами и цифрами", level="success",
                         display_func=st.success)
-                    st.dataframe(data["clear_text"].head(10))
-
-            st.sidebar.title("Модель")
-            if st.sidebar.checkbox("Настроить", key="model", on_change=clear_other_checkboxes, args=("model",)):
-                st.sidebar.subheader("Параметры векторизации")
-
-                def on_slider_change_max_df():
-                    st.write(f"Выбранное значение max_df: {st.session_state.slider_value_max_df}")
-                    value = st.session_state.slider_value_max_df
-                    return value
-
-                st.sidebar.slider(
-                    "max_df",
-                    min_value=0.0,
-                    max_value=1.0,
-                    value=0.8,
-                    step=0.1,
-                    key="slider_value_max_df",
-                    on_change=on_slider_change_max_df,
-                )
-
-                def on_slider_change_min_df():
-                    st.write(f"Выбранное значение min_df: {st.session_state.slider_value_min_df}")
-                    value = st.session_state.slider_value_min_df
-                    return value
-
-                st.sidebar.slider(
-                    "min_df",
-                    min_value=0.0,
-                    max_value=1.0,
-                    value=1.0,
-                    step=0.1,
-                    key="slider_value_min_df",
-                    on_change=on_slider_change_min_df,
-                )
-
-                def on_slider_change_max_features():
-                    st.write(f"Выбранное значение max_features: {st.session_state.max_features}")
-                    value = st.session_state.max_features
-                    return value
-
-                st.sidebar.slider(
-                    "max_features",
-                    min_value=1000,
-                    max_value=30000,
-                    value=9000,
-                    step=500,
-                    key="max_features",
-                    on_change=on_slider_change_max_features,
-                )
-
-                def on_sublinear_tf():
-                    st.write(f"Выбранное значение sublinear_tf: {st.session_state.sublinear_tf}")
-                    value = st.session_state.sublinear_tf
-                    return value
-
-                st.sidebar.radio(
-                    "sublinear_tf",
-                    [True, False],
-                    index=0,
-                    key="sublinear_tf",
-                    on_change=on_sublinear_tf,
-                )
-
-                def on_ngram():
-                    st.write(f"Выбранное значение ngram_range: {st.session_state.ngram_range}")
-                    value = st.session_state.ngram_range
-                    return value
-
-                st.sidebar.radio(
-                    "ngram_range",
-                    [(1, 1), (1, 2), (1, 3)],
-                    index=0,
-                    key="ngram_range",
-                    on_change=on_ngram,
-                )
-
-                hyperparameters = {}
-                hyperparameters["max_df"] = on_slider_change_max_df()
-                hyperparameters["min_df"] = on_slider_change_min_df()
-                hyperparameters["max_features"] = on_slider_change_max_features()
-                hyperparameters["smooth_idf"] = on_sublinear_tf()
-                hyperparameters["sublinear_tf"] = on_ngram()
-                log_and_display(f"гиперпараметры модели {hyperparameters}", level="info")
-
-                st.sidebar.subheader("Выбор метрики близости")
-
-                def on_distance():
-                    st.write(f"Метрика: {st.session_state.distance}")
-                    value = st.session_state.distance
-                    return value
-
-                st.sidebar.radio(
-                    "distance",
-                    ["models.Distance.COSINE", "models.Distance.EUCLID"],
-                    index=0,
-                    key="distance",
-                    on_change=on_distance,
-                )
-
-                qdrant_parameters = {}
-                distance = on_distance()
-                qdrant_parameters["distance"] = distance
-                log_and_display(f"гиперпараметры qdrant {qdrant_parameters}", level="info")
-
-                st.sidebar.subheader("Обучение модели")
-
-                if st.sidebar.button("Параметры"):
-                    st.success(f"Модель")
-                else:
-                    st.warning("Модель не определена")
-
-                if st.sidebar.button("Обучить"):
-                    st.success(f"Модель обучена")
-                else:
-                    st.warning("Не получилось")
-
-                if st.sidebar.button("Точность"):
-                    st.success(f"Точность %")
-                else:
-                    st.warning("Нет модели")
-
-            def text_form(key):
-                if "textarea" not in st.session_state:
-                    st.session_state.textarea = ""
-                st.text_area("Введите текст для проверки работы модели",
-                             height=200,
-                             value="",
-                             placeholder="Введите или скопируйте текст",
-                             key=key)
-
-            if st.sidebar.checkbox("Инференс", key="infer", on_change=clear_other_checkboxes, args=("infer",)):
-                text_form("textarea")
-
-                if st.button('Отправить текст'):
-                    if len(st.session_state.textarea):
-                        st.success('Текст отправлен в модель')
+                    st.dataframe(data.head(10))
+                    payload = {}
+                    payloads = data.to_dict("records")
+                    data_name = uploaded_file.name
+                    payload["datasets"] = {data_name: payloads}
+                    response = requests.post(f"{API_URL}/load_dataset", json=payload)
+                    if response.status_code == 201:
+                        log_and_display(f"{response.json()}", level="success", display_func=st.success)
+                        return response.json()
                     else:
-                        st.warning('Поле пустое')
-                if st.session_state.textarea:
-                    st.write(st.session_state.textarea)
-                    test = st.session_state.textarea
-                    # inference  = model(test)
-                    log_and_display("Предикт выполнен успешно", level="success")
+                        log_and_display(f"Ошибка при запросе API: {response.status_code}", level="error",
+                                        display_func=st.error)
 
-            if st.sidebar.checkbox("Повторить", key="repeat", on_change=clear_other_checkboxes, args=("repeat",)):
-                text_form("repeating")
-                if st.button('Отправить текст'):
-                    if len(st.session_state.repeating):
-                        st.success('Текст отправлен в модель')
-                    else:
-                        st.warning('Поле пустое')
-                if st.session_state.repeating:
-                    st.write(st.session_state.repeating)
-                    test = st.session_state.repeating
-                    # inference  = model(test)
-                    log_and_display("Предикт выполнен успешно", level="success")
+                    st.sidebar.title("Модель")
+                    if st.sidebar.checkbox("Настроить", key="model", on_change=clear_other_checkboxes, args=("model",)):
+                        st.sidebar.subheader("Параметры векторизации")
+
+                        def on_slider_change_max_df():
+                            st.write(f"Выбранное значение max_df: {st.session_state.slider_value_max_df}")
+                            value = st.session_state.slider_value_max_df
+                            return value
+
+                        st.sidebar.slider(
+                            "max_df",
+                            min_value=0.0,
+                            max_value=1.0,
+                            value=0.8,
+                            step=0.1,
+                            key="slider_value_max_df",
+                            on_change=on_slider_change_max_df,
+                        )
+
+                        def on_slider_change_min_df():
+                            st.write(f"Выбранное значение min_df: {st.session_state.slider_value_min_df}")
+                            value = st.session_state.slider_value_min_df
+                            return value
+
+                        st.sidebar.slider(
+                            "min_df",
+                            min_value=0.0,
+                            max_value=1.0,
+                            value=1.0,
+                            step=0.1,
+                            key="slider_value_min_df",
+                            on_change=on_slider_change_min_df,
+                        )
+
+                        def on_slider_change_max_features():
+                            st.write(f"Выбранное значение max_features: {st.session_state.max_features}")
+                            value = st.session_state.max_features
+                            return value
+
+                        st.sidebar.slider(
+                            "max_features",
+                            min_value=1000,
+                            max_value=30000,
+                            value=9000,
+                            step=500,
+                            key="max_features",
+                            on_change=on_slider_change_max_features,
+                        )
+
+                        def on_sublinear_tf():
+                            st.write(f"Выбранное значение sublinear_tf: {st.session_state.sublinear_tf}")
+                            value = st.session_state.sublinear_tf
+                            return value
+
+                        st.sidebar.radio(
+                            "sublinear_tf",
+                            [True, False],
+                            index=0,
+                            key="sublinear_tf",
+                            on_change=on_sublinear_tf,
+                        )
+
+                        def on_ngram():
+                            st.write(f"Выбранное значение ngram_range: {st.session_state.ngram_range}")
+                            value = st.session_state.ngram_range
+                            return value
+
+                        st.sidebar.radio(
+                            "ngram_range",
+                            [(1, 1), (1, 2), (1, 3)],
+                            index=0,
+                            key="ngram_range",
+                            on_change=on_ngram,
+                        )
+
+                        hyperparameters = {}
+                        hyperparameters["max_df"] = on_slider_change_max_df()
+                        hyperparameters["min_df"] = on_slider_change_min_df()
+                        hyperparameters["max_features"] = on_slider_change_max_features()
+                        hyperparameters["smooth_idf"] = on_sublinear_tf()
+                        hyperparameters["sublinear_tf"] = on_ngram()
+                        log_and_display(f"гиперпараметры модели {hyperparameters}", level="info")
+
+                        st.sidebar.subheader("Выбор метрики близости")
+
+                        def on_distance():
+                            st.write(f"Метрика: {st.session_state.distance}")
+                            value = st.session_state.distance
+                            return value
+
+                        st.sidebar.radio(
+                            "distance",
+                            ["models.Distance.COSINE", "models.Distance.EUCLID"],
+                            index=0,
+                            key="distance",
+                            on_change=on_distance,
+                        )
+
+                        qdrant_parameters = {}
+                        distance = on_distance()
+                        qdrant_parameters["distance"] = distance
+                        log_and_display(f"гиперпараметры qdrant {qdrant_parameters}", level="info")
+
+                        st.sidebar.subheader("Обучение модели")
+
+                        if st.sidebar.button("Параметры"):
+                            st.success(f"Модель")
+                        else:
+                            st.warning("Модель не определена")
+
+                        if st.sidebar.button("Обучить"):
+                            st.success(f"Модель обучена")
+                        else:
+                            st.warning("Не получилось")
+
+                        if st.sidebar.button("Точность"):
+                            st.success(f"Точность %")
+                        else:
+                            st.warning("Нет модели")
+
+                    def text_form(key):
+                        if "textarea" not in st.session_state:
+                            st.session_state.textarea = ""
+                        st.text_area("Введите текст для проверки работы модели",
+                                     height=200,
+                                     value="",
+                                     placeholder="Введите или скопируйте текст",
+                                     key=key)
+
+                    if st.sidebar.checkbox("Инференс", key="infer", on_change=clear_other_checkboxes, args=("infer",)):
+                        text_form("textarea")
+
+                        if st.button('Отправить текст'):
+                            if len(st.session_state.textarea):
+                                st.success('Текст отправлен в модель')
+                            else:
+                                st.warning('Поле пустое')
+                        if st.session_state.textarea:
+                            st.write(st.session_state.textarea)
+                            test = st.session_state.textarea
+                            # inference  = model(test)
+                            log_and_display("Предикт выполнен успешно", level="success")
+
+                    if st.sidebar.checkbox("Повторить", key="repeat", on_change=clear_other_checkboxes, args=("repeat",)):
+                        text_form("repeating")
+                        if st.button('Отправить текст'):
+                            if len(st.session_state.repeating):
+                                st.success('Текст отправлен в модель')
+                            else:
+                                st.warning('Поле пустое')
+                        if st.session_state.repeating:
+                            st.write(st.session_state.repeating)
+                            test = st.session_state.repeating
+                            # inference  = model(test)
+                            log_and_display("Предикт выполнен успешно", level="success")
+
 
 
 if __name__ == "__main__":
