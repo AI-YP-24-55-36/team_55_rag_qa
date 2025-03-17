8c58cef5ab192640747b4700e5fd41d01b0782b7
Lyudmila Teplova <lteplova@gmail.com>
2025-03-17 21:57:34 +0300

    add config and yml

--- notebooks/ipynb_bench/Teplova/cache_embed.py
+++ notebooks/ipynb_bench/Teplova/cache_embed.py
@@ -1,37 +1,46 @@
 import numpy as np
 import logging
 from pathlib import Path
-import os
+from load_config import load_config
+
+config = load_config()
+
+BASE_DIR = Path(config["paths"]["base_dir"])
+LOGS_DIR = BASE_DIR / config["paths"]["logs_dir"]
+GRAPHS_DIR = BASE_DIR / config["paths"]["graphs_dir"]
+EMBEDDINGS_DIR = BASE_DIR / config["paths"]["embeddings_dir"]
+
 
 logger = logging.getLogger('embed')
 logger.setLevel(logging.INFO)
 logger.propagate = False
 
-Path('./logs').mkdir(exist_ok=True)
-file_handler = logging.FileHandler('./logs/embed.log')
+# Path('./logs').mkdir(exist_ok=True)
+file_handler = logging.FileHandler(f'{LOGS_DIR}/embed.log')
 file_handler.setLevel(logging.INFO)
 
 # Форматирование логов
 formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
 file_handler.setFormatter(formatter)
-
-# Добавление обработчиков к логгеру
 logger.addHandler(file_handler)
 
-def generate_and_save_embeddings(texts, model, array_name, save_dir="embeddings"):
+
+def generate_and_save_embeddings(texts, model, array_name, save_dir=None):
+
+    # Используем директорию из параметра или из конфигурации
+    save_dir = Path(save_dir) if save_dir else EMBEDDINGS_DIR
 
     # Создаем директорию, если она не существует
-    if not os.path.exists(save_dir):
-        os.makedirs(save_dir)
+    save_dir.mkdir(exist_ok=True, parents=True)
 
     # Полный путь к файлу
-    file_path = os.path.join(save_dir, f"{array_name}.npy")
+    file_path = save_dir / f"{array_name}.npy"
 
     # Проверка на существование файла
-    if os.path.exists(file_path):
+    if file_path.exists():
         logger.info(f"Файл {file_path} уже существует. Загружаем существующие эмбеддинги.")
-
         return np.load(file_path)
+
     # Генерация новых эмбеддингов
     logger.info(f"Генерируем новые эмбеддинги для {array_name}...")
     vectors = model.encode(texts, show_progress_bar=False)
